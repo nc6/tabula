26eacce1d9ecfb36ea4c3eff932fbb0a6fad9f30
Nicholas Clarke <nc6@sanger.ac.uk>
2013-12-18 11:59:16 +0000

    Finish modularisation. Still not working :-(

--- Tabula/exec.hs
+++ Tabula/exec.hs
@@ -1,22 +1,20 @@
-module Tabula.Exec where
+module Tabula.Exec (exec) where
   import Prelude hiding (sequence)
 
   import Control.Concurrent (forkIO)
   import Control.Concurrent.STM (atomically)
   import Control.Concurrent.STM.TBMChan (newTBMChan)
+  import Control.Exception.Base (bracket)
   
   import qualified Data.ByteString.Lazy.Char8 as B
   import Data.Conduit
   import qualified Data.Conduit.Binary as DCB
   import Data.Conduit.TMChan (sourceTBMChan, sinkTBMChan)
-  import Data.List (isPrefixOf)
   import Data.Time.Clock
   import Data.Traversable (sequence)
 
   import Network.BSD (getHostName)
-  import Network.URL
 
-  import qualified System.Console.Readline as RL
   import System.Environment (getEnvironment)
   import System.Exit
   import System.IO
@@ -24,12 +22,44 @@ module Tabula.Exec where
   import System.Process
   import System.Posix.IO (fdToHandle, openFd, OpenMode(ReadWrite), defaultFileFlags)
   import System.Posix.Terminal 
+  import System.Posix.Types (Fd(..))
 
   import qualified Tabula.Record as Rec
   import qualified Tabula.Record.Console as Rec.Cons
-  import Tabula.Destination (makeEntry)
-  import qualified Tabula.Destination.File as FD
-  import qualified Tabula.Strings as S
+  import Tabula.Destination (Destination(), makeEntry)
+
+  exec :: Destination -> String -> IO ()
+  exec dest command = withRawPty $ \(ptym,ptys) -> do
+    hostName <- getHostName
+    workingDirectory <- getWorkingDirectory
+    priorEnv <- getEnvironment
+    startTime <- getCurrentTime
+    (_, _, hErr, ph) <- createProcess $ (shell command) { 
+          delegate_ctlc = True
+        , std_err = CreatePipe
+        , std_out = UseHandle ptys
+        , std_in = UseHandle ptys
+      }
+    snipOut <- tee 256 ptym stdout
+    snipErr <- sequence $ fmap (\h -> tee 256 h stderr) hErr
+    snipIn <- tee 256 stdin ptym
+    exitCode <- waitForProcess ph
+    endTime <- getCurrentTime
+    posteriorEnv <- getEnvironment
+    let record = Rec.record (Rec.Cons.ConsoleRecord 
+                    command
+                    hostName
+                    workingDirectory
+                    priorEnv
+                    posteriorEnv
+                    startTime
+                    endTime
+                    (Just snipIn)
+                    (Just snipOut)
+                    snipErr
+                    (exitCodeInt exitCode)
+                )
+    makeEntry dest record
 
   tee :: Int -> Handle -> Handle -> IO B.ByteString
   tee bytes from to = do
@@ -41,4 +71,34 @@ module Tabula.Exec where
 
   exitCodeInt :: ExitCode -> Int
   exitCodeInt ExitSuccess = 0
-  exitCodeInt (ExitFailure a) = a 
+  exitCodeInt (ExitFailure a) = a
+
+  withRawPty :: ((Handle, Handle) -> IO a) -> IO a
+  withRawPty action = do
+      cTerm <- getControllingTerminalName >>= 
+            \a -> openFd a ReadWrite Nothing defaultFileFlags
+      parentAttrs <- getTerminalAttributes cTerm
+      bracket (initialise parentAttrs cTerm)
+              (finalise parentAttrs cTerm)
+              action
+    where
+      initialise :: TerminalAttributes -> Fd -> IO (Handle, Handle)
+      initialise parentAttrs cTerm = do
+        (ptym, ptys) <- openPseudoTerminal
+        setTerminalAttributes ptym parentAttrs Immediately
+        setTerminalAttributes cTerm (withoutModes rawModes parentAttrs) Immediately
+        hm <- fdToHandle ptym
+        hs <- fdToHandle ptys
+        return (hm, hs)
+
+      finalise :: TerminalAttributes -> Fd -> (Handle, Handle) -> IO ()
+      finalise parentAttrs cTerm (hm, hs) = do
+        hClose hm
+        hClose hs
+        setTerminalAttributes cTerm parentAttrs Immediately
+
+      rawModes = [ProcessInput, KeyboardInterrupts, ExtendedFunctions, 
+                  EnableEcho, InterruptOnBreak, MapCRtoLF, IgnoreBreak, 
+                  IgnoreCR, MapLFtoCR, CheckParity, StripHighBit, 
+                  StartStopOutput, MarkParityErrors, ProcessOutput]
+      withoutModes modes tty = foldl withoutMode tty modes
--- tabula.hs
+++ tabula.hs
@@ -2,25 +2,12 @@ module Main where
   import Prelude hiding (sequence)
 
   import Data.List (isPrefixOf)
-  import Data.Time.Clock
-  import Data.Traversable (sequence)
 
-  import Network.BSD (getHostName)
   import Network.URL
 
   import qualified System.Console.Readline as RL
-  import System.Environment (getEnvironment)
-  import System.Exit
-  import System.IO
-  import System.Posix.Directory (getWorkingDirectory)
-  import System.Process
-  import System.Posix.IO (fdToHandle, openFd, OpenMode(ReadWrite), defaultFileFlags)
-  import System.Posix.Terminal 
 
   import Tabula.Exec
-  import qualified Tabula.Record as Rec
-  import qualified Tabula.Record.Console as Rec.Cons
-  import Tabula.Destination (makeEntry)
   import qualified Tabula.Destination.File as FD
   import qualified Tabula.Strings as S
 
@@ -37,23 +24,6 @@ module Main where
 
   readEvalPrintLoop :: IO ()
   readEvalPrintLoop = do
-      pty <- do
-        parentTerminal <- getControllingTerminalName >>= 
-                          \a -> openFd a ReadWrite Nothing defaultFileFlags
-        sttyp <- getTerminalAttributes parentTerminal
-        (a, b) <- openPseudoTerminal
-        let rawModes = [ProcessInput, KeyboardInterrupts, ExtendedFunctions, 
-                        EnableEcho, InterruptOnBreak, MapCRtoLF, IgnoreBreak, 
-                        IgnoreCR, MapLFtoCR, CheckParity, StripHighBit, 
-                        StartStopOutput, MarkParityErrors, ProcessOutput]
-            sttym = withoutModes rawModes sttyp
-            withoutModes modes tty = foldl withoutMode tty modes
-        setTerminalAttributes b sttym Immediately
-        setTerminalAttributes a sttym Immediately
-        a' <- fdToHandle a
-        b' <- fdToHandle b
-        return (a',b')
-
       maybeLine <- RL.readline "% "
       case maybeLine of
         Nothing     -> return () -- EOF / control-d
@@ -63,43 +33,8 @@ module Main where
           readEvalPrintLoop
         Just line -> do 
           RL.addHistory line
-          eval pty line
+          exec (FD.fileDestination "typescript") line
           readEvalPrintLoop
-
-  eval :: (Handle, Handle) -> String -> IO ()
-  eval pty command = do
-      let (ptym, ptys) = pty
-      hostName <- getHostName
-      workingDirectory <- getWorkingDirectory
-      priorEnv <- getEnvironment
-      startTime <- getCurrentTime
-      (_, _, hErr, ph) <- createProcess $ (shell command) { 
-            delegate_ctlc = True
-          , std_err = CreatePipe
-          , std_out = UseHandle ptys
-          , std_in = UseHandle ptys
-        }
-      snipOut <- tee 256 ptym stdout
-      snipErr <- sequence $ fmap (\h -> tee 256 h stderr) hErr
-      snipIn <- tee 256 stdin ptym
-      exitCode <- waitForProcess ph
-      endTime <- getCurrentTime
-      posteriorEnv <- getEnvironment
-      let record = Rec.record (Rec.Cons.ConsoleRecord 
-                      command
-                      hostName
-                      workingDirectory
-                      priorEnv
-                      posteriorEnv
-                      startTime
-                      endTime
-                      (Just snipIn)
-                      (Just snipOut)
-                      snipErr
-                      (exitCodeInt exitCode)
-                  )
-      makeEntry fileDest record
-    where fileDest = FD.fileDestination "typescript"
 
   evalMetaCommand :: String -> IO ()
   evalMetaCommand command = case command of
