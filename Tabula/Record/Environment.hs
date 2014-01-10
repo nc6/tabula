2b2e67fe30d6303bebfe80cf370c5b7abefd34a0
Nicholas Clarke <nc6@sanger.ac.uk>
2014-01-10 15:47:58 +0000

    Add 'indifferentMerge' functionality.
    This will be useful for implementing issue #8, although I should also
    write a less indifferent merge that complains if the environment has
    changed.

--- Tabula/Record/Environment.hs
+++ Tabula/Record/Environment.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
 -- | Types and functions dealing with environment variables.
-module Tabula.Record.Environment (Env, EnvChange, diff) where
+module Tabula.Record.Environment (Env, EnvChange, diff, indifferentMerge) where
 
   import Control.Arrow ((***))
   import Control.Monad (join)
 
   import Data.Aeson (FromJSON, ToJSON)
+  import Data.List (foldl')
   import qualified Data.Map.Strict as Map
 
   import GHC.Generics (Generic)
@@ -35,3 +36,20 @@ module Tabula.Record.Environment (Env, EnvChange, diff) where
         Map.intersectionWithKey Modify f t
     in insertions ++ deletions ++ modifications
 
+  {- | Merge in changes to an environment to produce a new environment.
+      Note that this version of merge is indifferent to whether the changes
+      are actually appropriate to the environment they are being applied to:
+
+       - A modify will work even if the old value is different from the one
+      present, or there is no value present.
+       - An insert will act as a modify if a value is already there.
+       - A delete will not care if the value it's deleting does not exist.
+
+  -}
+  indifferentMerge :: EnvChange -> Env -> Env
+  indifferentMerge changes old = Map.toList $ 
+      foldl' appDelta (Map.fromList old) changes 
+    where
+      appDelta env (Insert key value) = Map.insert key value env
+      appDelta env (Modify key _ value) = Map.insert key value env
+      appDelta env (Delete key) = Map.delete key env
