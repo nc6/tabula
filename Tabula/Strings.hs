1c78cf3920816d7d209693a54f90988fd7ba6746
Nicholas Clarke <nc6@sanger.ac.uk>
2014-01-23 13:56:58 +0000

    Remove Tabula.Strings
    Wasn't using it anyway. Possibly should later, but removed for now.

--- Tabula/Strings.hs
+++
@@ -1,5 +0,0 @@
-module Tabula.Strings where
-
-  metaCommandNotFound :: String -> String
-  metaCommandNotFound a = "Metacommand " ++ a ++ 
-    " not implemented. Use :help to get a list of commands."
