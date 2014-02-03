06249c6e2bf8d4229568be02536f71cd69f5a938
Nicholas Clarke <nc6@sanger.ac.uk>
2014-02-03 16:19:47 +0000

    Bump version number.

--- Tabula/Options.hs
+++ Tabula/Options.hs
@@ -70,7 +70,7 @@ module Tabula.Options (
   --------------- Parsers ------------------
 
   version :: Parser (a -> a)
-  version = infoOption "Tabula version 0.1.1.1"
+  version = infoOption "Tabula version 0.1.2.0"
     (  long "version"
     <> help "Print version information" )
   
